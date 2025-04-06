323a7f9a2f34ab85b658dd6ca630b48233de9674
ntondama <ntondama31@gmail.com>
2025-04-06 13:14:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,29 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-southeast-2a"
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "allow_ssh" {
@@ -39,7 +57,7 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_instance" "auto_hostname" {
-  ami                    = "ami-0df4b2961410d4cff" # Amazon Linux 2 in ap-southeast-2 (Sydney)
+  ami                    = "ami-0df4b2961410d4cff" # Amazon Linux 2 for Sydney
   instance_type          = "t2.micro"
   key_name               = "NTsydney"  # Replace with your actual key name
   subnet_id              = aws_subnet.public_subnet.id
